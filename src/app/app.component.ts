3f14a035c26ef6a5f8a1886c3a32e902851ee3c1
cdandrango3 <cdandrango@uce.edu.ec>
2022-12-27 11:08:22 -0500

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'examenangular';
+  datasend(item:string){
+    console.log(item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { InitialComponent } from './component/initial/initial.component';
+import  {HttpClientModule} from '@angular/common/http';
+import { GifComponentComponent } from './component/gif-component/gif-component.component';
+import { GiffinderComponent } from './component/giffinder/giffinder.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialComponent,
+    GifComponentComponent,
+    GiffinderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
